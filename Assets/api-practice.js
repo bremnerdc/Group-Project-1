e963939f065d5848d9c3982a58f76e3557b2fcf4
Grant Smith <grantnsmith@gmail.com>
2020-04-11 13:57:47 -0700

    Create ajax calling to OpenCage that grabs latitude and longitude based off of user input (e.g. city, zip).

--- Assets/api-practice.js
+++ Assets/api-practice.js
@@ -1,28 +1,60 @@
 
+$(document).ready(function() {
 
-function testingAPI() {
-queryURL = "https://api.yelp.com/v3/businesses/search?term=pizza?location=Seattle";
-
+// AJAX CALL FROM USER INPUT (CITY NAME/STATE) TO GRAB LATITUDE AND LONGITUDE 
+var userSearch = "98103"
+var cityNameQueryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + userSearch + "&key=3cc36a63992d44a2af35f53240a19709";
 
     $.ajax({
-        url: queryURL,
-        headers: {
-            'Authorization':'Bearer T70YHIWJA6ASHZ612TjqJG1AJWR0Xn-9fjik3GHwDfnCr0UGGgOFmh9Juyj9tQ7QjmxaaQhQqa3NyWMyhFxNMx4RH-MW3AVOF-iDhMWQf3C76tW-_kd8K-NuYt6PXnYx'
-        },
-          method: 'GET',
+        url: cityNameQueryURL,
+        method: 'GET',
     }).done(function(response){
         console.log(response);
+        var lat = response.results[0].geometry.lat;
+        console.log("Lat: " + lat);
+        var long = response.results[0].geometry.lng;
+        console.log("Long: " + long);
+
+
     })
-}
-
-testingAPI();
-
-// testing
+});
 
 
 
+    // function testingAPI() {
+// queryURL = "https://developers.zomato.com/api/v2.1/locations?query=denver&lat=39&lon=-104";
+
+//     $.ajax({
+//         url: queryURL,
+//         headers: {
+//             'user-key':'bd33447588fb9817164b8c1f3167fb68'
+//         },
+//           method: 'GET',
+//     }).done(function(response){
+//         console.log(response);
+//         var entityType = response.location_suggestions[0].entity_type;
+//         console.log(entityType);
+//         var entityID = response.location_suggestions[0].entity_id;
+//         console.log(entityID);
+//         var restaurantQueryURL = "https://developers.zomato.com/api/v2.1/locations?query"
+
+// // Second Ajax call for restaurant list
+//         $.ajax({
+//             url: restaurantQueryURL,
+//             headers: {
+//                 'user-key':'bd33447588fb9817164b8c1f3167fb68'
+//             },
+//               method: 'GET',
+//         }).done(function(response){
+//             console.log(response);
+//         }
+//     })
+
+   
 
 
+// testingAPI();
 
+// });
 
 
