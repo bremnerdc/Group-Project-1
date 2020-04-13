134d3cc93c6c166fa41fe046375c84c5f6c4abb3
Sabah Syed <sabisyed@gmail.com>
2020-04-13 06:26:29 -0700

    working on merges

--- Assets/api-practice.js
+++ Assets/api-practice.js
@@ -1,1 +1,19 @@
+const GOOGLE_API_KEY = 'AIzaSyAuAw09DnunxYW9E9SjcKvYanUYhWcOdLA';
 
+
+
+
+var url = reverseGeoLocationUrl(70, -90);
+
+console.log(url)
+
+
+
+
+function reverseGeoLocationUrl(lat, lon){
+    const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    const query = 'latlng=' + lat + ',' + lon
+    const key = '&key=' + GOOGLE_API_KEY;
+
+    return baseURL + query + key;
+}
