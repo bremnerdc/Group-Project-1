7450114d88c71edc7a54f6ee87b9ba1ed76cdbcc
bremnerdc <bremnerdc@gmail.com>
2020-04-14 21:21:07 -0700

    added ajax call to script.js, and other jQuery

---
+++ Assets/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  // CLICK EVENT ON SUBMIT BUTTON - AJAX CALL INSIDE
+  $("#searchBtn").on("click", function (event) {
+      event.preventDefault();
+      console.log("button clicked");
+      var userSearch = $(".validate").val();
+      console.log(userSearch);
+      var cityNameQueryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + userSearch + "&key=3cc36a63992d44a2af35f53240a19709";
+  
+  // AJAX CALL FROM USER INPUT (CITY NAME/ZIP CODE) TO GRAB LATITUDE AND LONGITUDE 
+      $.ajax({
+          url: cityNameQueryURL,
+          method: 'GET',
+      }).done(function(response){
+          console.log(response);
+          var lat = response.results[0].geometry.lat;
+          console.log("Lat: " + lat);
+          var long = response.results[0].geometry.lng;
+          console.log("Long: " + long);
+          var location = lat + "," + long;
+        
+          //var meal_takeaway = $("#takeaway");
+          //var meal_delivery = $("#delivery");
+          var queryURL ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=meal_delivery&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
+                         https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=50.724489,-3.527855&radius=10000&type=meal_delivery&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c
+
+
+        //if (meal_delivery.clicked === true) {
+        //queryURL =
+        //} 
+        // else if (meal_takeaway.clicked === true) {
+        // queryURL ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=meal_takeaway&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
+        // } else {
+        // queryURL ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=meal_delivery&type=meal_takeaway&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
+        // }
+
+        $.ajax({
+          url: queryURL,
+          dataType: "jsonp",
+          method: 'GET',
+      }).done(function(response){
+          console.log(response);
+          for (var i = 0; i < 20; i++) {
+            console.log(response.results[i].name)
+            
+              var restaurantDiv = $("<div>");
+              var card = $("<div>").addClass("card grey-grey darken-1");
+              var body = $("<div>").addClass("card-content");
+              var title = $("<h5>").addClass("card-title").text(results.name[i]);
+              var p1 = $("<p>").addClass("card-text").text("address:" + results.vicinity[i]);
+            
+              // merge together and put on page
+              restaurantDiv.append(card.append(body.append(title, p1)));
+              $("#restaurant.row").append(col);
+        }
+
+          });
+        });
+        });
+      });
+
+
+     //may have to loop through both results.name & results.vicinity...
+
+   
