a65658e6bb834417119d3405f29649e2d2b4507d
Sabah Syed <sabisyed@gmail.com>
2020-04-16 14:15:33 -0700

    Added new account and shopping cart button

--- Assets/api-practice.js
+++ Assets/api-practice.js
@@ -1,59 +1,63 @@
-    
-    //const GOOGLE_API_KEY = 'AIzaSyAuAw09DnunxYW9E9SjcKvYanUYhWcOdLA';
-    const GEOCODING_API_KEY = "1ac03d1663a44a149344a23b47ce483f";
-    var lat, lang;
-   
-    function reverseGeoLocationUrl(lat, lon){
-        const baseURL = 'https://api.opencagedata.com/geocode/v1/json?'
-        const query = 'q=' + lat + ',' + lon
-        const key = '&key=' + GEOCODING_API_KEY;
-       // return baseURL + query + key;
-        console.log(baseURL+query+key);
-        $.ajax({
-        url: "https://api.opencagedata.com/geocode/v1/json?" + 'q=' + lat + ',' + lon + '&key=' + GEOCODING_API_KEY ,
-        method: "GET"
-        }).then(function(response) {
-         console.log(response.results[0].components.county);
-         var houseNo = response.results[0].components.house_number;
-         var county = response.results[0].components.county;
-         var road =response.results[0].components.road;
-         var state = response.results[0].components.state_code;
-         var postalCode = response.results[0].components.postcode;
-         var country = response.results[0].components.country;
-         var formatted = response.results[0].formatted;
-         console.log("Address is: "  + formatted);
-         $("#demo").append(houseNo , road ,county ,state, postalCode, country);
+$('.modal').modal();
 
+//const GOOGLE_API_KEY = 'AIzaSyAuAw09DnunxYW9E9SjcKvYanUYhWcOdLA';
+const GEOCODING_API_KEY = "1ac03d1663a44a149344a23b47ce483f";
+var lat, lang;
+
+
+
+function reverseGeoLocationUrl(lat, lon) {
+    const baseURL = 'https://api.opencagedata.com/geocode/v1/json?'
+    const query = 'q=' + lat + ',' + lon
+    const key = '&key=' + GEOCODING_API_KEY;
+    // return baseURL + query + key;
+    console.log(baseURL + query + key);
+    $.ajax({
+        url: "https://api.opencagedata.com/geocode/v1/json?" + 'q=' + lat + ',' + lon + '&key=' + GEOCODING_API_KEY,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.results[0].components.county);
+        var houseNo = response.results[0].components.house_number;
+        var county = response.results[0].components.county;
+        var road = response.results[0].components.road;
+        var state = response.results[0].components.state_code;
+        var postalCode = response.results[0].components.postcode;
+        var country = response.results[0].components.country;
+        var formatted = response.results[0].formatted;
+        console.log("Address is: " + formatted);
+        // $("#demo").append(houseNo , road ,county ,state, postalCode, country);
+        console.log("The address is " + ($("#locationBtn").value = formatted));
     });
 }
-    var x = document.getElementById("demo");
+var x = document.getElementById("demo");
 
-    function getLocation() {
-        event.preventDefault();
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-        
+function getLocation() {
+    event.preventDefault();
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
 
-    function showPosition(position) {
-        // x.innerHTML = "Latitude: " + position.coords.latitude +
-        //     "<br>Longitude: " + position.coords.longitude;
-        var lat = position.coords.latitude;
-        var lang = position.coords.longitude;
-        reverseGeoLocationUrl(lat,lang);
-    }
-    // function initMap() {
-    //     // The location of Uluru
-    //     var uluru = {lat: 47.850787, lng: -122.9855};
-    //     // The map, centered at Uluru
-    //     var map = new google.maps.Map(
-    //         document.getElementById('map'), {zoom: 4, center: uluru});
-    //     // The marker, positioned at Uluru
-    //     var marker = new google.maps.Marker({position: uluru, map: map});
-    //   }
-    function contactDetails(){
-        
-    }
+}
+
+function showPosition(position) {
+    // x.innerHTML = "Latitude: " + position.coords.latitude +
+    //     "<br>Longitude: " + position.coords.longitude;
+    var lat = position.coords.latitude;
+    var lang = position.coords.longitude;
+    reverseGeoLocationUrl(lat, lang);
+}
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 47.850787, lng: -122.9855};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+function contactDetails() {
+
+}
+
