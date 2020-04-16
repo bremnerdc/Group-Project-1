85bdd15fcc3a6f2a88f4b62cdeaa387872c8ef2b
bremnerdc <bremnerdc@gmail.com>
2020-04-16 14:40:57 -0700

    update js and index

--- Assets/api-practice.js
+++ Assets/api-practice.js
@@ -1,26 +1,28 @@
 
+$(document).ready(function() {
 
-function testingAPI() {
-queryURL = "https://api.yelp.com/v3/businesses/search?term=pizza?location=Seattle";
+// CLICK EVENT ON SUBMIT BUTTON - AJAX CALL INSIDE
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    console.log("button clicked");
+    var userSearch = $("#location-search").val();
+    console.log(userSearch);
+    var cityNameQueryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + userSearch + "&key=3cc36a63992d44a2af35f53240a19709";
 
-
+// AJAX CALL FROM USER INPUT (CITY NAME/ZIP CODE) TO GRAB LATITUDE AND LONGITUDE 
     $.ajax({
-        url: queryURL,
-        headers: {
-            'Authorization':'Bearer T70YHIWJA6ASHZ612TjqJG1AJWR0Xn-9fjik3GHwDfnCr0UGGgOFmh9Juyj9tQ7QjmxaaQhQqa3NyWMyhFxNMx4RH-MW3AVOF-iDhMWQf3C76tW-_kd8K-NuYt6PXnYx'
-        },
-          method: 'GET',
+        url: cityNameQueryURL,
+        method: 'GET',
     }).done(function(response){
         console.log(response);
+        var lat = response.results[0].geometry.lat;
+        console.log("Lat: " + lat);
+        var long = response.results[0].geometry.lng;
+        console.log("Long: " + long);
+        var location = lat + "," + long;
+        return location;
+
     })
-}
-
-testingAPI();
-
-
-
-
-
-
-
-
+  });
+});
+  
