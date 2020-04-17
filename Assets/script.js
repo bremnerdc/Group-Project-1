73c372b8605e6c400fe7cbb8c79c0abb0528dcd5
Grant Smith <grantnsmith@gmail.com>
2020-04-16 18:45:22 -0700

    Finalized MVP.

--- Assets/script.js
+++ Assets/script.js
@@ -2,52 +2,62 @@
 $(document).ready(function() {
 
     // CLICK EVENT ON SUBMIT BUTTON - AJAX CALL INSIDE
-    $("#searchBtn").on("click", function (event) {
+        $("#searchBtn").on("click", function (event) {
         event.preventDefault();
         $("#restaurants").empty();
-        // console.log("button clicked");
         var userSearch = $(".validate").val();
-        // console.log(userSearch);
         var cityNameQueryURL = "https://api.opencagedata.com/geocode/v1/json?q=" + userSearch + "&key=3cc36a63992d44a2af35f53240a19709";
     
     // AJAX CALL FROM USER INPUT (CITY NAME/ZIP CODE) TO GRAB LATITUDE AND LONGITUDE 
         $.ajax({
             url: cityNameQueryURL,
             method: 'GET',
+        }).fail(function(response){
+            $(".modal").modal();
+            $("#failed-search-modal").modal('open');
+        
         }).done(function(response){
-            // console.log(response);
             var lat = response.results[0].geometry.lat;
-            // console.log("Lat: " + lat);
             var long = response.results[0].geometry.lng;
-            // console.log("Long: " + long);
             var location = lat + "," + long;
         
             var queryURL;
 
-            // RADIO BUTTONS FOR TAKEAWAY AND DELIVERY     
-            if ($("#delivery").prop("checked")) {
+    // RADIO BUTTONS FOR TAKEAWAY AND DELIVERY     
+        if ($("#delivery").prop("checked")) {
                 queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=meal_delivery&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
-            }
-            else if ($("#takeaway").prop("checked")) {
+        }
+        else if ($("#takeaway").prop("checked")) {
                 queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=meal_takeaway&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
-            } else {
+        } else {
                 queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=10000&type=restaurant&opennow=true&key=AIzaSyApNMnp_rkqJzxJaSxvpit0MvEhVw1vm7c";
-            };
+        };
   
-          $.ajax({
+        $.ajax({
             url: queryURL,
             method: 'GET',
+        }).fail(function(response){
+            $(".modal").modal();
+            $("#failed-search-modal").modal('open');
+        
         }).done(function(response){
-            console.log(response);
-            for (var i = 0; i < response.results.length; i++) {
-              var name = response.results[i].name;
-              var rating = response.results[i].rating;
-              var address = response.results[i].vicinity;
+
+    // MODAL IF CALL GETS ZERO RESULTS
+        if (response.status == "ZERO_RESULTS") {
+                $(".modal").modal();
+                $("#no-results-modal").modal('open');
+        }
+
+    // CREATING VARIABLES FROM JSON OBJECT RESULTS 
+        for (var i = 0; i < response.results.length; i++) {
+        var name = response.results[i].name;
+        var rating = response.results[i].rating;
+        var address = response.results[i].vicinity;
               
 
-        // Creating Forecast HTML elements
+    // CREATING RESTAURANT RESULT ELEMENTS
         var restaurantColDiv = $("<div class='col s12 m6 l6'></div>");
-        var restaurantDiv = $("<div class='card-panel grey-grey'></div>");
+        var restaurantDiv = $("<div class='card-panel teal lighten-5'></div>");
         var restaurantNameEl = $("<span class='card-title'  id='restaurant-header'>" + name + "</span>");
         var openNowEl = $("<p>OPEN NOW</p>");
         var ratingEl = $("<p>Google rating: " + rating + " / 5" + "</p>");
@@ -57,7 +67,7 @@ $(document).ready(function() {
         var link = "https://www.google.com/maps/dir/?api=1&destination=" + encodedAddress
         var directionsLink = $("<a target='_blank' href=" + link + ">Directions</a>");
 
-        // Appending forecast elements to the HTML
+    // APPENDING RESTAURANT ELEMENTS TO PAGE
         restaurantDiv.append(restaurantContentDiv);
         restaurantColDiv.append(restaurantDiv);
         restaurantContentDiv.append(restaurantNameEl);
@@ -67,14 +77,10 @@ $(document).ready(function() {
         restaurantContentDiv.append(directionsLink);
         $("#restaurants").append(restaurantColDiv);
 
-
-              
-                
-          }
-  
-            });
-          });
-          });
-        });
+        }
+      });
+    });
+  });     
+});
   
   
