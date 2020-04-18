73cc3152bcf671a920615c8c54af84436abce17d
Sabah Syed <sabisyed@gmail.com>
2020-04-18 00:20:39 -0700

    Added maps with home and restaurant markers

--- Assets/api-practice.js
+++ Assets/api-practice.js
@@ -3,9 +3,6 @@ $('.modal').modal();
 //const GOOGLE_API_KEY = 'AIzaSyAuAw09DnunxYW9E9SjcKvYanUYhWcOdLA';
 const GEOCODING_API_KEY = "1ac03d1663a44a149344a23b47ce483f";
 var lat, lang;
-
-
-
 function reverseGeoLocationUrl(lat, lon) {
     const baseURL = 'https://api.opencagedata.com/geocode/v1/json?'
     const query = 'q=' + lat + ',' + lon
@@ -34,7 +31,8 @@ var x = document.getElementById("demo");
 function getLocation() {
     event.preventDefault();
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+         navigator.geolocation.getCurrentPosition(showPosition);
+         
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -42,22 +40,26 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    // x.innerHTML = "Latitude: " + position.coords.latitude +
-    //     "<br>Longitude: " + position.coords.longitude;
-    var lat = position.coords.latitude;
-    var lang = position.coords.longitude;
+     lat = position.coords.latitude;
+     lang = position.coords.longitude;
+    console.log(lat);
+    console.log(lang);
     reverseGeoLocationUrl(lat, lang);
 }
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 47.850787, lng: -122.9855};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+
+    console.log("value of lat and long outside function is :" + lat + "," + lang );
+    var map;
+    function initMap() {
+        console.log(lat);
+        console.log(lang);
+        //reverseGeoLocationUrl(lat, lang);
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat:45.45, lng:-100.989 },
+        zoom: 15
+        
+        });
+        }
+    
 function contactDetails() {
 
-}
-
+}   
